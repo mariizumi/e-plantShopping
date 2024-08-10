ae944b3447752092259b4c5094c212117813bef3
Mai Ariizumi <mariizumi6450@gmail.com>
2024-08-10 18:18:50 -0400

    Made program work

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,7 +21,7 @@ const CartItem = ({ onContinueShopping }) => {
         onContinueShopping(e);
     };
 
-    const handleCheckoutShopping = (e) => {
+    const handleCheckoutShopping = () => {
         alert('Functionality to be added for future reference');
     };
 
@@ -73,7 +73,7 @@ const CartItem = ({ onContinueShopping }) => {
             <div className="continue_shopping_btn">
                 <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1">Checkout</button>
+                <button className="get-started-button1" onClick={() => handleCheckoutShopping()}>Checkout</button>
             </div>
         </div>
     );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,7 @@ function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addedToCart, setAddedToCart] = useState({});
+  const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   const plantsArray = [
@@ -263,6 +264,20 @@ function ProductList() {
     }));
   };
 
+  const getCartItemCount = () => {
+    let total = 0;
+    for (const item of cart) {
+      total += item.quantity;
+    }
+    return total;
+  };
+
+  const getAddButtonText = (product) => {
+    return cart.find(item => item.name == product.name) ? 'Added to Cart' : 'Add to Cart';
+  };
+
+  const isPlantInCart = (product) => cart.find(item => item.name == product.name);
+
   return (
     <div>
       <div className="navbar" style={styleObj}>
@@ -279,12 +294,14 @@ function ProductList() {
 
         </div>
         <div style={styleObjUl}>
-          <div> 
+          <div>
             <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a>
           </div>
-          <div style={{position: 'relative'}}>
-            <div style={{position: 'absolute', top: '20px', left: '20px', width: '30px', 'text-align': 'center'}}>12</div>
-            <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a>
+          <div style={{ position: 'relative' }}>
+            <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+              <div className="cart-quantity">{getCartItemCount()}</div>
+              <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>
+            </a>
           </div>
         </div>
       </div>
@@ -298,7 +315,9 @@ function ProductList() {
                   <div className="product-card" key={plantIndex}>
                     <img className="product-image" src={plant.image} alt={plant.name} />
                     <div className="product-title">{plant.name}</div>
-                    <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                    <div className="product-cost">{plant.cost}</div>
+                    <div className="product-description">{plant.description}</div>
+                    <button className="product-button" onClick={() => handleAddToCart(plant)} style={{ 'background-color': isPlantInCart(plant) ? '#777' : '#4caf50' }}>{getAddButtonText(plant)}</button>
                   </div>
                 ))}
               </div>
